ee378875a5f6554f122d692e5e4f5d0c896a7732
Bukola Omojowo <bomojowo@gmail.com>
2021-06-02 13:22:35 +0100

    Added 3 more test cases to 1st challenge

--- challenges/check-if-equal.js
+++ challenges/check-if-equal.js
@@ -22,7 +22,10 @@ function isEqual(a,b){
 //test cases:
 //===========
 console.log('isEqual(4,4):',isEqual(4, 4), 'should be True');
-console.log('isEqual(0, 0)', isEqual(0, 0), 'should be True');
-console.log('isEqual(-4, -4)', isEqual(-4, -4), 'should be True');
-console.log('sEqual(4, -4)',isEqual(4, -4), 'should be False');
-console.log('isEqual(0, 1)', isEqual(0, 1), 'should be False');
+console.log('isEqual(0, 0):', isEqual(0, 0), 'should be True');
+console.log('isEqual(-4, -4):', isEqual(-4, -4), 'should be True');
+console.log('sEqual(4, -4):',isEqual(4, -4), 'should be False');
+console.log('isEqual(0, 1):', isEqual(0, 1), 'should be False');
+console.log('isEqual(100, -100):', isEqual(100, -100), 'should be false');
+console.log('isEqual(0, 00):', isEqual(0, 00), 'should be True');
+console.log('isEqual(4, 44:)', isEqual(4, 44), 'should be False')
