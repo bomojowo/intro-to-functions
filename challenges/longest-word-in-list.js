dc1121c4e21f3dc7160d3cc6f702e798814582d2
Bukola Omojowo <bomojowo@gmail.com>
2021-06-02 15:18:28 +0100

    Completed challenge 1:longest word in list

---
+++ challenges/longest-word-in-list.js
@@ -0,0 +1,35 @@
+// function:
+// =========
+// name: longestInList
+// parameter: words: list of strings
+// return type: string
+
+// pseudo code:
+// ============
+function longestInList(words){
+    // set longest to first string in words
+    let longest = words[0];
+    //console.log(longest)
+    // for each string word in words
+    for (let word of words){
+        // 	if word has more characters than longest
+        //console.log(word)
+        if (word.length > longest.length){
+            //set longest to word 
+            longest = word;
+            //console.log(longest)
+        }
+    }
+    // return longest
+    return longest;
+}
+
+
+// test cases:
+// ===========
+console.log(longestInList(["England", "Wales", "Scotland", "Northern Ireland"]), 'should be:"Northern Ireland"')
+console.log(longestInList(["the", "quick", "brown", "fox"]), 'should be "quick"')
+console.log(longestInList(["hello", "hi", "greetings", "hey"]), 'should be "greetings"')
+console.log(longestInList(["Orange", "Apple", "Grape", "Grapefruit"]), 'should be "Grapefruit"')
+console.log(longestInList(["Bee", "Wasp", "Fly", "Spider", "Centipede", ]), 'should be "Centipede"')
+console.log(longestInList(["Pen", "Sharpner", "Rubber", "Ruler"]), 'should be: "Sharpner"')
