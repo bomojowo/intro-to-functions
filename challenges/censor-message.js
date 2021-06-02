09b655a17b55157fe094097dbc01bfd98e806a6d
Bukola Omojowo <bomojowo@gmail.com>
2021-06-02 22:42:21 +0100

    Added 3 extra test cases to challenge 2

--- challenges/censor-message.js
+++ challenges/censor-message.js
@@ -38,10 +38,11 @@ function censorMessage(message, banList){
     
 }
 
-
-
 // test cases:
 // ===========
-//console.log(censorMessage("don't mess with cats", ["mess"]), 'should be "don\'t ***** with cats"')
-//censorMessage("Are you sure that this is safe", ["are", "is"]) should be "***** you sure that this ***** safe"
+console.log(censorMessage("don't mess with cats", ["mess"]), 'should be "don\'t ***** with cats"')
+console.log(censorMessage("Are you sure that this is safe", ["are", "is"]), 'should be "***** you sure that this ***** safe"')
 console.log(censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]), 'should be "I CANNOT ***** *****"')
+console.log(censorMessage("the Bat sat on the mat", ["bat", "sat", "mat"]), 'should be "the ***** ***** on the *****"')
+console.log(censorMessage("Melon or Watermelon ?", ["melon", "watermelon"]), 'should be "***** or ***** ?"')
+console.log(censorMessage("What time should I come over ?", ["what", "I"]), 'should be "***** time should ***** come over ?"')
