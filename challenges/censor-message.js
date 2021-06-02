2161a28033500e5fdd59713b6d1fec0df64b4f69
Bukola Omojowo <bomojowo@gmail.com>
2021-06-02 18:50:03 +0100

    Completed challenge 2

---
+++ challenges/censor-message.js
@@ -0,0 +1,47 @@
+// function:
+// =========
+// name: censorMessage
+// parameter: message: string
+// parameter: banList: list of strings
+// return type: string
+// pseudo code:
+// ============
+function censorMessage(message, banList){
+    //console.log(banList)
+    // set rawWords to the list of strings created by separating the string message into sub-strings at every space (' ')
+    let rawWords = message.split(' ');
+    //console.log(rawWords)
+    // set scrubbedWords to an empty list
+    let scrubbedWords = [];
+    // for each string word in rawWords
+    for (stringWord of rawWords){
+        // 	set lowercaseWord to lowercase(word)
+        let lowercaseWord = stringWord.toLowerCase();
+        //console.log(lowercaseWord)
+        //  if lowercaseWord is contained in the list banList
+        if (banList.includes(lowercaseWord)){
+            //  insert the string '*****' at the end of scrubbedWord
+            scrubbedWords.push('*****');
+            //console.log(scrubbedWords)
+        } else{
+        //else
+        //   insert word at the end of scrubbedWords
+            scrubbedWords.push(stringWord)
+            //console.log(scrubbedWords)
+        }
+        //set scrubbedMessage to the string created by joining together every string in scrubbedWords with a space (' ') as a join character
+        scrubbedMessage = scrubbedWords.join(' ');
+        //console.log(scrubbedMessage)
+    }
+    // return scrubbedMessage
+    return scrubbedMessage;
+    
+}
+
+
+
+// test cases:
+// ===========
+//console.log(censorMessage("don't mess with cats", ["mess"]), 'should be "don\'t ***** with cats"')
+//censorMessage("Are you sure that this is safe", ["are", "is"]) should be "***** you sure that this ***** safe"
+console.log(censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]), 'should be "I CANNOT ***** *****"')
